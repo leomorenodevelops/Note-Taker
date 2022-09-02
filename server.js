0e3708cae47ec00cae26cdc64be92194a3830add
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-09-02 15:19:39 -0600

    Set route for API

--- server.js
+++ server.js
@@ -12,3 +12,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
+
+// Setting routes for APIs
+// Notes get saved and joins them in db.json
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Develop/db/db.json'));
+});
