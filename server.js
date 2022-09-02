33c320c57d1691b554b1dc6e288adef83da031ff
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-09-02 15:08:50 -0600

    created middleware

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ const { DH_CHECK_P_NOT_SAFE_PRIME } = require('constants');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
